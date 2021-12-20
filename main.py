61c36a7adaafb9c457d061ca1e8551c5f5d99f04
= <=>
2021-12-20 02:07:55 +0200

    ...

---
+++ main.py
@@ -0,0 +1,15 @@
+class Main:
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+	def plus(self):
+		return self.a + self.b
+
+	def minus(self):
+		return self.a - self.b
+
+	def multiply(self):
+		return self.a * self.b
+		
+	def divide(self):
+		return self.a // self.b
